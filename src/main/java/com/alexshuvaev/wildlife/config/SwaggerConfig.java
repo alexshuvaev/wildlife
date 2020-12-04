f021d8db181cba506da2470bcd057a83090f3e8b
alexshuvaev <alexshuvaevdev@gmail.com>
2020-12-05 00:28:46 +0300

    implemented configuration, liquibase, swagger

---
+++ src/main/java/com/alexshuvaev/wildlife/config/SwaggerConfig.java
@@ -0,0 +1,52 @@
+package com.alexshuvaev.wildlife.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger.web.OperationsSorter;
+import springfox.documentation.swagger.web.UiConfiguration;
+import springfox.documentation.swagger.web.UiConfigurationBuilder;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+
+import java.time.LocalDate;
+import java.util.Collections;
+
+@EnableSwagger2
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.alexshuvaev.wildlife"))
+                .paths(PathSelectors.any())
+                .build()
+                .directModelSubstitute(LocalDate.class, java.sql.Date.class)
+                .apiInfo(apiDetails());
+    }
+
+    @Bean
+    public UiConfiguration uiConfig() {
+        return UiConfigurationBuilder
+                .builder()
+                .operationsSorter(OperationsSorter.METHOD)
+                .build();
+    }
+
+    private ApiInfo apiDetails() {
+        return new ApiInfo(
+                "Wildlife Sanctuary API",
+                "Api",
+                "1.0",
+                "Free dto use",
+                new springfox.documentation.service.Contact("Alexandr Shuvaev", "", "alexshuvaevdev@gmail.com"),
+                "",
+                ""
+                , Collections.emptyList());
+    }
+}
